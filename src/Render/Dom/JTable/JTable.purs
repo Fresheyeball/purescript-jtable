5f62e27f9e811b6de459bb12013d890220d4d95c
Isaac Shapira <fresheyeball@gmail.com>
2015-01-24 17:59:34 -0700

    decoder ring of destiny cant detect the semantic

--- src/Render/Dom/JTable/JTable.purs
+++ src/Render/Dom/JTable/JTable.purs
@@ -1,30 +1,35 @@
 module Render.Dom.JTable where
 
+import Data.Argonaut.Decode (DecodeJson)
+import Data.Argonaut.JCursor 
 import Data.Argonaut
 import Data.Either
+import Data.Maybe
+import Data.StrMap
+import Data.Tuple
 import Text.Smolder.Markup
-
-data JSemantic = Integral
-               | Fractional
-               | Date
-               | DateTime
-               | Time
-               | Interval
-               | Text
-               | Bool
-               | Percent
-               | Currency
-               | NA
 
 type Level = Number
 
-type JPath = [String]
+data JTree = JMap (StrMap JTree) | JList [JTree] | JLeaf String
 
-data ColumnOrdering = InOrdering | CustomOrdering (JPath -> JPath -> Ordering)
+data ColumnOrdering = M | CustomOrdering (JCursor -> JCursor -> Ordering)
 
 data TableStyle = TableStyle {
   table   :: Level -> Markup -> Markup,
-  cell    :: JSemantic -> Markup -> Markup,
-  head    :: JPath -> Markup -> Markup,
+  cell    :: forall a. a -> Markup -> Markup,
+  head    :: JCursor -> Markup -> Markup,
   row     :: Markup -> Markup
 }
+
+foldJsonToJTree :: Data.Argonaut.Core.Json -> JTree
+foldJsonToJTree = let
+    null    = const (JLeaf "") -- null
+    boolean = show >>> JLeaf   -- boolean  
+    number  = show >>> JLeaf   -- number 
+    string  = JLeaf            -- string   ↙array                            ↙object
+  in foldJson null boolean number string ((<$>) foldJsonToJTree >>> JList) ((<$>) foldJsonToJTree >>> JMap)
+  -- what gives? http://lpaste.net/119159
+
+instance decodeJTree :: DecodeJson JTree where
+  decodeJson = Right <<< foldJsonToJTree
--- test/Render/Dom/JTable/JTable.Test.purs
+++ test/Render/Dom/JTable/JTable.Test.purs
@@ -1,29 +1,37 @@
 module Render.Dom.JTable.Test where
 
-import Test.StrongCheck
+import Data.Argonaut
+import qualified Test.StrongCheck as S 
 import Debug.Trace
+import Test.Unit
 
 import Render.Dom.JTable
 
 sampleJson = """
-{
-  "userId": 8927524,
-  "profile": {
-    "name":   "Mary Jane",
-    "age":    29,
-    "gender": "female"
-  },
-  "comments": [{
-      "id":       "F2372BAC",
-      "text":     "I concur.",
-      "replyTo":  [9817361, "F8ACD164F"],
-      "time":     "2015-02-03"
-    }, {
-      "id":       "GH732AFC",
-      "replyTo":  [9654726, "A44124F"],
-      "time":     "2015-03-01"
-  }]
-}
+  {
+    "userId": 8927524,
+    "profile": {
+      "name":   "Mary Jane",
+      "age":    29,
+      "gender": "female"
+    },
+    "comments": [{
+        "id":       "F2372BAC",
+        "text":     "I concur.",
+        "replyTo":  [9817361, "F8ACD164F"],
+        "time":     "2015-02-03"
+      }, {
+        "id":       "GH732AFC",
+        "replyTo":  [9654726, "A44124F"],
+        "time":     "2015-03-01"
+    }]
+  }
 """ :: String
 
-main = trace "wowzers!!"
+-- parseTest = do
+--   p <- decodeJson sampleJson
+--   assert p ==
+
+
+main = do 
+  trace "json parses generically"
