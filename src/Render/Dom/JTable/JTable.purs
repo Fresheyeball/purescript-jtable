9b03d780a8a5442a3b5faa6819a772f4f67cdc24
Isaac Shapira <fresheyeball@gmail.com>
2015-01-26 09:51:00 -0700

    eq

--- src/Render/Dom/JTable/JTable.purs
+++ src/Render/Dom/JTable/JTable.purs
@@ -8,7 +8,7 @@ import Data.Argonaut.Core ( Json()
                           , JArray()
                           , JObject() )
 import Data.Argonaut.Decode (DecodeJson)
-import Data.Argonaut.JCursor 
+import Data.Argonaut.JCursor
 import Data.Argonaut
 import Data.Either
 import Data.Maybe
@@ -20,41 +20,36 @@ import Text.Smolder.Markup
 
 type Level = Number
 
-data JSemantic = JSString  JString 
-               | JSNumber  JNumber 
-               | JSBoolean JBoolean 
-               | JSArray   JArray 
-               | JSObject  JObject 
+data JSemantic = JSString  JString
+               | JSNumber  JNumber
+               | JSBoolean JBoolean
                | JSNull    JNull
 
-data JTree = JMap (StrMap JTree) 
-           | JList [JTree] 
+data JTree = JMap (StrMap JTree)
+           | JList [JTree]
            | JLeaf JSemantic
 
 foldJTree :: forall a. (JSemantic -> a -> a) -> a -> JTree -> a
-foldJTree f i = go where    
+foldJTree f i = go where
   go (JMap  sm) = fold  (\i' _ b  -> foldJTree f i' b) i sm
   go (JList ts) = foldr (\b    i' -> foldJTree f i' b) i ts
   go (JLeaf s ) = f s i
 
-(/+) :: forall a. (Show a) => String -> a -> String 
+(/+) :: forall a. (Show a) => String -> a -> String
 (/+) x a = x ++ " (" ++ show a ++ ")"
 
 instance showJSemantic :: Show JSemantic where
   show (JSString  a) = "JSString"  /+ a
   show (JSNumber  a) = "JSNumber"  /+ a
   show (JSBoolean a) = "JSBoolean" /+ a
-  show (JSArray   a) = "JSArray"   /+ a
-  show (JSObject  a) = "JSObject"  /+ a
   show (JSNull    a) = "JSNull"    /+ a
 
 instance eqJSemantic :: Eq JSemantic where
   (==) (JSString  a) (JSString  a') = a == a'
   (==) (JSNumber  a) (JSNumber  a') = a == a'
   (==) (JSBoolean a) (JSBoolean a') = a == a'
-  (==) (JSArray   a) (JSArray   a') = a == a'
-  (==) (JSObject  a) (JSObject  a') = a == a'
   (==) (JSNull    a) (JSNull    a') = a == a'
+  (==) _             _              = false 
   (/=) x y = not $ x == y
 
 instance showJTree :: Show JTree where
@@ -66,16 +61,16 @@ instance eqJTree :: Eq JTree where
   (==) (JMap  sm) (JMap  sm') = sm == sm'
   (==) (JList ts) (JList ts') = ts == ts'
   (==) (JLeaf s ) (JLeaf s' ) = s  == s'
-  (==) _          _           = false 
+  (==) _          _           = false
   (/=) x y = not $ x == y
 
 type JFold f a = f a -> (a -> f a) -> Json -> f a
 
 parseToJTree :: Json -> JTree
 parseToJTree = go
-  
+
   where
-  
+
   intoLeaf :: forall a. JFold [] a -> (a -> JSemantic) -> Json -> JTree
   intoLeaf f c = f [] (\x -> [x]) >>> head >>> c >>> JLeaf
 
--- test/Render/Dom/JTable/JTable.Test.purs
+++ test/Render/Dom/JTable/JTable.Test.purs
@@ -43,37 +43,43 @@ sampleJson = """
 sampleJTree = JMap m
   where
   m  = empty
-    # insert "userId"     ( JLeaf (JSNumber 8927524)     )
-    >>> insert "profile"  ( JMap  profile                )
-    >>> insert "comments" ( JList comments               )
+    #   insert "userId"   ( JLeaf (JSNumber 8927524)      )
+    >>> insert "profile"  ( JMap  profile                 )
+    >>> insert "comments" ( JList comments                )
   profile = empty
-    #   insert "name"     ( JLeaf (JSString "Mary Jane") )
-    >>> insert "age"      ( JLeaf (JSNumber 29)          )
-    >>> insert "gender"   ( JLeaf (JSString "female")    )
+    #   insert "name"     ( JLeaf (JSString "Mary Jane")  )
+    >>> insert "age"      ( JLeaf (JSNumber 29)           )
+    >>> insert "gender"   ( JLeaf (JSString "female")     )
   comments = [JMap c0, JMap c1]
   c0 = empty
-    #   insert "id"       ( JLeaf (JSString "F2372BAC")  )
-    >>> insert "text"     ( JLeaf (JSString "I concur.") )
+    #   insert "id"       ( JLeaf (JSString "F2372BAC")   )
+    >>> insert "text"     ( JLeaf (JSString "I concur.")  )
     >>> insert "replyTo"  ( JList [JLeaf (JSNumber 9817361), JLeaf (JSString "F8ACD164F") ])
-    >>> insert "time"     ( JLeaf (JSString "2015-02-03"))
+    >>> insert "time"     ( JLeaf (JSString "2015-02-03") )
   c1 = empty
-    #   insert "id"       ( JLeaf (JSString "GH732AFC")  )
+    #   insert "id"       ( JLeaf (JSString "GH732AFC")   )
     >>> insert "replyTo"  ( JList [JLeaf (JSNumber 9654726), JLeaf (JSString "A44124F")   ])
-    >>> insert "time"     ( JLeaf (JSString "2015-03-01"))
+    >>> insert "time"     ( JLeaf (JSString "2015-03-01") )
 
 jsonEqJTree :: String -> JTree -> Boolean
 jsonEqJTree x y = case jsonParser x >>= decodeJson of
   Left  _     -> false
   Right jtree -> jtree == y
 
-checkEq :: JTree -> Boolean
-checkEq x = x == x && not (x /= x)
+checkEq :: forall a. (Eq a) => a -> Boolean
+checkEq x = (x == x)
+     && not (x /= x)
 
 main = do
+  trace "JSemantic test start"
+
+  trace "eq of JSemantic"
+  quickCheck' 10 (checkEq :: JSemantic -> Boolean)
+
   trace "JTree test start"
 
-  trace "eq"
-  quickCheck' 10 checkEq
+  trace "eq of JTree"
+  quickCheck' 10 (checkEq :: JTree -> Boolean)
 
-  trace "sampleJson"
+  trace "sampleJson matches sampleJTree"
   assert $ jsonEqJTree sampleJson sampleJTree
