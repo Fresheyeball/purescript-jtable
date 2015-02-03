50beccd1a006172459cbcb12e2074d836b1fc220
Isaac Shapira <fresheyeball@gmail.com>
2015-02-03 08:41:23 -0700

    oh the genialities

--- src/JTable.purs
+++ src/JTable.purs
@@ -17,26 +17,58 @@ import Debug.Spy (spy)
 type Level = Number
 type Row   = [[Markup]]
 
+data Uniformity = Heterogeneous | Homogeneous
+
+instance showUniformity :: Show Uniformity where
+  show Heterogeneous 
+    = "Heterogeneous"
+  show Homogeneous 
+    = "Homogeneous"
+
+instance eqUniformity :: Eq Uniformity where
+
+  (==) Homogeneous Homogeneous = true
+  (==) Heterogeneous Heterogeneous = true
+  (==) _ _ = false
+  (/=) x y = not $ x == y 
+
 normalizeCursor :: JCursor -> JCursor
 normalizeCursor jc = case jc of
+
   JCursorTop             -> JCursorTop
   JField f (JIndex _ jc) -> normalizeCursor $ JField f jc
   JField f jc            -> JField f $ normalizeCursor jc
   JIndex _ jc            -> normalizeCursor jc
 
-parseNRender :: String -> Either String Markup
-parseNRender x = renderJTable <$> jsonParser x
-
 collect :: Tuple JCursor JsonPrim -> Map JCursor [JsonPrim] -> Map JCursor [JsonPrim]
 collect (Tuple jc' jp) m = let jc = normalizeCursor jc'
-                           in if member jc m 
+                           in if member jc m                               
                               then alter (\mv -> (<>) [jp] <$> mv) jc m 
                               else insert jc [jp] m 
+
+uniform :: [JsonPrim] -> Uniformity
+uniform = fst <<< foldr f (Tuple Homogeneous 0)
+  
+  where
+
+  f _  (Tuple Heterogeneous x) = Tuple Heterogeneous x
+  f jp (Tuple _ 0)             = Tuple Homogeneous $ testPrim $ primToJson jp  
+  f jp (Tuple _ i)             = Tuple 
+    (if i == i' then Homogeneous else Heterogeneous) i'
+    where i' = testPrim $ primToJson jp  
+
+  testPrim jp | isNull    jp = 1
+              | isString  jp = 2
+              | isBoolean jp = 3
+              | isNumber  jp = 4  
 
 -- renderJTable :: Json -> Markup
 -- renderJTable json = toPrims json # flip foldr empty \(Tuple jc v) m ->
 --   let 
 --     jc' = normalizeCursor jc
 --   in if member jc' m then 
+
+parseNRender :: String -> Either String Markup
+parseNRender x = renderJTable <$> jsonParser x
 
 renderJTable _ = td $ text "foo"
--- test/JTable.Test.purs
+++ test/JTable.Test.purs
@@ -10,15 +10,36 @@ import Data.Foldable (foldr)
 import Data.Array (nub, sort)
 
 import Test.StrongCheck
-import Test.StrongCheck.Gen
 import Test.QuickCheck.Tuple
 
 import Debug.Trace
 import Debug.Spy
 
 import Test.JTable.Arb
+import Control.Apply ((*>))
 
 import JTable
+
+sampleJson = """
+{
+  "userId": 8927524,
+  "profile": {
+    "name":   "Mary Jane",
+    "age":    29,
+    "gender": "female"
+  },
+  "comments": [{
+      "id":       "F2372BAC",
+      "text":     "I concur.",
+      "replyTo":  [9817361, "F8ACD164F"],
+      "time":     "2015-02-03"
+    }, {
+      "id":       "GH732AFC",
+      "replyTo":  [9654726, "A44124F"],
+      "time":     "2015-03-01"
+  }]
+}
+""" :: String
 
 section = trace <<< (<>) "\n" <<< flip (<>) "\n"
 
@@ -29,17 +50,51 @@ stringCursor (JIndex i jc) = "JIndex " <> show i <> " (" <> stringCursor jc <> "
 
 checkNormalizeJCursor :: JCursor -> Result
 checkNormalizeJCursor jc =
+  
   check (normalizeCursor jc) <?> "Normalize JCursor: " <> stringCursor jc
+  
   where
+  
   check JCursorTop     = true
   check (JField _ jc') = check jc'
   check (JIndex _ _  ) = false
 
 checkCollect :: [Tuple JCursor JsonPrim] -> Result
-checkCollect x = xs == xs' <?> "Check Collect: " <> show x
+checkCollect x = xs == xs' 
+
+  <?> "Check Collect: " <> show x   <> "\n"
+  <>  "xs:            " <> show xs  <> "\n"
+  <>  "xs':           " <> show xs' <> "\n"
+  
   where 
-  xs  = sort <<< keys   $ foldr collect empty x
-  xs' = sort <<< nub    $ normalizeCursor <<< fst <$> x
+  
+  xs  = sort <<< keys $ foldr collect empty x
+  xs' = sort <<< nub  $ normalizeCursor <<< fst <$> x
+
+checkUniform :: JsonPrim -> JsonPrim -> JsonPrim -> Result
+checkUniform jp jp' jp'' = let 
+    
+    testPrim jp | isNull    (primToJson jp) = 1
+                | isString  (primToJson jp) = 2
+                | isBoolean (primToJson jp) = 3
+                | isNumber  (primToJson jp) = 4  
+
+    homo      = uniform [ jp, jp,  jp   ]
+    hetro     = uniform [ jp, jp', jp'' ]
+    arbHetro  = testPrim jp   /= testPrim jp' 
+             || testPrim jp'  /= testPrim jp'' 
+             || testPrim jp'' /= testPrim jp 
+
+  in (if arbHetro 
+      then homo == Homogeneous && hetro == Heterogeneous 
+      else homo == Homogeneous)
+
+    <?> "Uniform: " <> show jp   <> "\n"
+    <>  "->       " <> show jp'  <> "\n"
+    <>  "->       " <> show jp'' <> "\n"
+
+murf = section "murf" *> case jsonParser sampleJson of
+  Right x -> print <<< foldr collect empty <<< toPrims $ x
 
 init = do
 
@@ -48,3 +103,6 @@ init = do
 
   trace "Collect to Map"
   quickCheck checkCollect
+
+  trace "Uniformity"
+  quickCheck checkUniform
